f636fb6ee4093319a4ceaa66e88b1d17bb02857f
siddp <siddp@DESKTOP-E3QJHPR.mshome.net>
2021-01-29 11:50:12 -0500

    First Commit

---
+++ src/main/java/com/boot/rest/api/bookdemo/SbRestApiBookDemoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.rest.api.bookdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbRestApiBookDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbRestApiBookDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boot/rest/api/bookdemo/SbRestApiBookDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.rest.api.bookdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbRestApiBookDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
